4dd03ef0d74611bf05f06c43cc2ff27dd25a697d
Ingram Weeks <ingram.weeks@gmail.com>
2021-10-02 16:08:56 +0100

    taste the rainbow

--- pio/ws2812/led_matrix.c
+++ pio/ws2812/led_matrix.c
@@ -34,25 +34,30 @@ void init_test_image()
 void init_test_image_diagonal()
 {
     static Colour start = RED;
-    static Colour finish = BLUE;
+    static Colour middle = BLUE;
+    static Colour finish = GREEN;
 
     static uint8_t start_row = 0;
     static uint8_t finish_row = 31;
 
     Colour pixel;
-    Colour increment;
+    Colour gradient_one, gradient_two;
 
     for (int j = 0; j < COLUMNS; j++)
     {
-        increment.red = (finish.red - start.red) / ROWS;
-        increment.green = (finish.green - start.green) / ROWS;
-        increment.blue = (finish.blue - start.blue) / ROWS;
+        gradient_one.red = (middle.red - start.red) / ROWS;
+        gradient_one.green = (middle.green - start.green) / ROWS;
+        gradient_one.blue = (middle.blue - start.blue) / ROWS;
+
+        gradient_two.red = (finish.red - start.red) / ROWS;
+        gradient_two.green = (finish.green - start.green) / ROWS;
+        gradient_two.blue = (finish.blue - start.blue) / ROWS;
 
         for(int i = start_row; i < ROWS; i++)
         {
-            pixel.red = (increment.red * (i - start_row)) + start.red;
-            pixel.green = (increment.green * (i - start_row)) + start.green;
-            pixel.blue = (increment.blue * (i - start_row)) + start.blue;
+            pixel.red = (gradient_one.red * (i - start_row)) + start.red;
+            pixel.green = (gradient_one.green * (i - start_row)) + start.green;
+            pixel.blue = (gradient_one.blue * (i - start_row)) + start.blue;
 
             image[i][j] = (uint32_t)((pixel.green) << 16) |
                         (uint32_t)((pixel.red) << 8) |
@@ -62,9 +67,9 @@ void init_test_image_diagonal()
 
         for (int i = 0; i < start_row + 1; i++)
         {
-            pixel.red = (increment.red * i) + start.red;
-            pixel.green = (increment.green * i) + start.green;
-            pixel.blue = (increment.blue * i) + start.blue;
+            pixel.red = (gradient_two.red * i) + start.red;
+            pixel.green = (gradient_two.green * i) + start.green;
+            pixel.blue = (gradient_two.blue * i) + start.blue;
 
             image[(start_row) - i][j] = (uint32_t)((pixel.green) << 16) |
                         (uint32_t)((pixel.red) << 8) |
@@ -76,9 +81,15 @@ void init_test_image_diagonal()
         start_row++;
     } else {
         start_row = 0;
-        Colour temp = start;
+        
+        Colour temp1, temp2;
+
+        temp1 = start;
+        temp2 = middle;
+
         start = finish;
-        finish = temp;
+        middle = temp1;
+        finish = temp2;
     }
 
     if(finish_row < ROWS)
